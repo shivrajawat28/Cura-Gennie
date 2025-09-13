3a7759118872b56aa90c601d875c2c00a25bae95
Shiv Rajawat <shivrajawat@Shivs-MacBook-Air-2.local>
2025-09-13 10:52:49 +0530

    updated title and description in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'CG',
+  description: 'Created with Code Vaidya',
   generator: 'v0.app',
 }
 
